d2a5486bb61b776a23f2602834135db2c677ace4
Nate-OBrien <nathan.obrien@mcvts.org>
2025-02-19 10:33:50 -0500

    combined

--- main.cpp
+++ main.cpp
@@ -5,19 +5,36 @@
 * Extra: 
 */
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
 
 using namespace std;
 
 void clear();
+vector<int> getRandomPos(int size);
 
 int main(){
+    srand(time(NULL));
+
     cout << "\033[31m" << endl;
-    string gameBoard[16] = {"#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",};
-    for (string piece : gameBoard){
-        cout << piece;
+    string gameBoard[32][64];
+
+    for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 64; j++){
+            gameBoard[i][j] = "#"; 
+            cout<< gameBoard[i][j]; 
+        }
+        cout<<endl; 
     }
 
-    cout << "\033[0m" << endl;
+    vector<int> randPos = getRandomPos(64);
+
+    for (int item : randPos){
+        cout << "\n" << item << endl;
+    }
+
+    cout << "\n "<< "\033[0m" << endl;
     return 0;
 }
 
@@ -29,4 +46,13 @@ void clear(){
     #elif defined (__APPLE__)
         system("clear");
     #endif
+}
+
+vector<int> getRandomPos(int size){
+    vector<int> arr (2);
+    for (int i = 0; i < 2; i++){
+        arr.at(i) = rand() % size;
+    }
+
+    return arr;
 }

762dfaeb699b7de9e43ac33d567684faa4c9a1c9
Nate-OBrien <nathan.obrien@mcvts.org>
2025-02-19 09:39:29 -0500

    clear

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void clear();
+
 int main(){
 
 
     return 0;
+}
+
+void clear(){
+    #if defined _WIN32
+        system("CLS");
+    #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+        system("clear");
+    #elif defined (__APPLE__)
+        system("clear");
+    #endif
 }
